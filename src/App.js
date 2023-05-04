9ff2670ab05166fa29105a77e722e4deb27856c8
CodeConnects <codeconnects@gmail.com>
2023-05-04 11:02:23 -0400

    add 5 monsters and display their names

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,33 +7,33 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Scottamoosh'
+      monster1: {
+        name: 'Frankenstein',
+      },
+      monster2: {
+        name: 'Dracula',
+      },
+      monster3: {
+        name: 'Zombie',
+      },
+      monster4: {
+        name: 'Mummy',
+      },
+      monster5: {
+        name: 'Werewolf',
+      },
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello {this.state.name}
-          </p>
-          <button onClick={() => {
-            this.setState(
-              () => {
-                return {
-                  name: 'Scott'
-                };
-              },
-              () => {
-                console.log(this.state.name);
-              }
-            );
-          }}>
-            Change Name
-          </button>
-        </header>
+        <h1>Monsterdex</h1>
+        <h2>Monster 1: {this.state.monster1.name}</h2>
+        <h2>Monster 2: {this.state.monster2.name}</h2>
+        <h2>Monster 3: {this.state.monster3.name}</h2>
+        <h2>Monster 4: {this.state.monster4.name}</h2>
+        <h2>Monster 5: {this.state.monster5.name}</h2>
       </div>
     );
   }
