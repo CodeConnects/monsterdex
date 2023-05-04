0197ac1e18e38370890a2c724324185985683425
CodeConnects <codeconnects@gmail.com>
2023-05-04 11:28:23 -0400

    add monsters to array and map them to html

--- src/App.js
+++ src/App.js
@@ -7,21 +7,23 @@ class App extends Component {
     super();
 
     this.state = {
-      monster1: {
-        name: 'Frankenstein',
-      },
-      monster2: {
-        name: 'Dracula',
-      },
-      monster3: {
-        name: 'Zombie',
-      },
-      monster4: {
-        name: 'Mummy',
-      },
-      monster5: {
-        name: 'Werewolf',
-      },
+      monsters: [
+        {
+          name: 'Frankenstein',
+        },
+        {
+          name: 'Dracula',
+        },
+        {
+          name: 'Zombie',
+        },
+        {
+          name: 'Mummy',
+        },
+        {
+          name: 'Werewolf',
+        },
+      ],
     };
   }
 
@@ -29,11 +31,11 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Monsterdex</h1>
-        <h2>Monster 1: {this.state.monster1.name}</h2>
-        <h2>Monster 2: {this.state.monster2.name}</h2>
-        <h2>Monster 3: {this.state.monster3.name}</h2>
-        <h2>Monster 4: {this.state.monster4.name}</h2>
-        <h2>Monster 5: {this.state.monster5.name}</h2>
+        {
+          this.state.monsters.map((monster) => {
+            return <h3>{monster.name}</h3>;
+          })
+        }
       </div>
     );
   }
