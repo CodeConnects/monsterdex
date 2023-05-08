3fe16bf76345448049438d13b8f8eea8718d9125
CodeConnects <codeconnects@gmail.com>
2023-05-08 18:12:12 -0400

    add filter box to search Pokemon

--- src/App.js
+++ src/App.js
@@ -26,6 +26,16 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Pokedexer</h1>
+        <input className='search-box' type='search' placeholder='Search Pokemon' 
+          onChange={(event) => {
+            const filteredPokemon = this.state.pokemon.filter((poke) => {
+              return poke.name.includes(event.target.value.toLowerCase());
+            });
+            this.setState(() => { 
+              return { pokemon: filteredPokemon };
+            });
+          }}
+        />
         {
           this.state.pokemon.map((poke) => {
             poke.name = poke.name[0].toUpperCase() + poke.name.slice(1);
