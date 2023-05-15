f9ffe16e5550606777527a77ea8ee07d7fbeb432
CodeConnects <codeconnects@gmail.com>
2023-05-15 09:54:48 -0400

    break out card list into a component

---
+++ src/components/card-list/card-list.comp.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+class CardList extends Component {
+
+  render() {
+    const { pokemon } = this.props;
+    return (
+      <div>
+        {
+          pokemon.map((poke) => {
+            poke.name = poke.name[0].toUpperCase() + poke.name.slice(1);
+            return <h3 key={poke.id}>{poke.name}</h3>;
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default CardList;
