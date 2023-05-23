ff93ee96a8139454a7123e3b31a6486830c1e844
CodeConnects <codeconnects@gmail.com>
2023-05-23 10:19:44 -0400

    build out the Pokemon cards from api info

--- src/components/card-list/card-list.comp.jsx
+++ src/components/card-list/card-list.comp.jsx
@@ -6,14 +6,24 @@ class CardList extends Component {
 
   render() {
     const { pokemon } = this.props;
+    
     return (
-      <div>
-        {
-          pokemon.map((poke) => {
-            poke.name = poke.name[0].toUpperCase() + poke.name.slice(1);
-            return <h3 key={poke.url}>{poke.name}</h3>;
-          })
-        }
+      <div className="card-list">
+        {pokemon.map((poke) => {
+          const pokeID = poke.url.slice(34, -1);
+          const properName = poke.name[0].toUpperCase() + poke.name.slice(1);
+
+          return (
+            <div className='card-wrap' key={pokeID}>
+              <img 
+                alt={`Pokemon ${properName}`}
+                name={properName}
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeID}.png`} 
+              />
+              <h3>{properName}</h3>
+            </div>
+          )
+        })}
       </div>
     );
   }
