c39978bceae51350d53aa3616591a0c588d14e7a
CodeConnects <codeconnects@gmail.com>
2023-06-04 20:33:05 -0400

    convert from Class to Functional Component

--- src/components/card-list/card-list.comp.jsx
+++ src/components/card-list/card-list.comp.jsx
@@ -1,24 +1,15 @@
-import { Component } from "react";
-
 import './card-list.styles.scss';
 import PokemonCard from "../pokemon-card/pokemon-card.comp";
 
-class CardList extends Component {
+const CardList = ({ pokemon }) => (
+  <div className="card-list">
+    {pokemon.map((poke) => {
 
-  render() {
-    const { pokemon } = this.props;
-    
-    return (
-      <div className="card-list">
-        {pokemon.map((poke) => {
-
-          return (
-            <PokemonCard poke={poke} key={poke.name} />
-          )
-        })}
-      </div>
-    );
-  }
-}
+      return (
+        <PokemonCard poke={poke} key={poke.name} />
+      )
+    })}
+  </div>
+);
 
 export default CardList;
