f7ca8a75ca0b4c6a3747c44b9869ba5c12625832
CodeConnects <codeconnects@gmail.com>
2023-06-04 20:25:55 -0400

    convert from Class to Functional Component

--- src/components/search-box/search-box.comp.jsx
+++ src/components/search-box/search-box.comp.jsx
@@ -1,23 +1,15 @@
-import { Component } from "react";
-
 import './search-box.styles.scss';
 
-class SearchBox extends Component {
-
-  render() {
-    const { onChangeHandler, placeholder, className } = this.props;
-    return (
-      <div>
-        <input 
-          type='search'
-          name={ className }
-          className={ `search-box ${className}` }
-          placeholder={ placeholder }
-          onChange={ onChangeHandler }
-        />
-      </div>
-    );
-  }
-}
+const SearchBox = ({ onChangeHandler, placeholder, className }) => (
+  <div>
+    <input 
+      type='search'
+      name={ className }
+      className={ `search-box ${className}` }
+      placeholder={ placeholder }
+      onChange={ onChangeHandler }
+    />
+  </div>
+);
 
 export default SearchBox;
