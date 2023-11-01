ab83ca14a7616e138bd1ca49235bf277ac25deff
CodeConnects <codeconnects@gmail.com>
2023-11-01 16:52:38 -0400

    add const for image url with Shiny version

--- src/components/pokemon-card/pokemon-card.comp.jsx
+++ src/components/pokemon-card/pokemon-card.comp.jsx
@@ -4,16 +4,17 @@ const PokemonCard = ({ poke }) => {
   const pokeID = poke.url.slice(34, -1);
   const properName = poke.name[0].toUpperCase() + poke.name.slice(1);
   const imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeID}.png`;
+  const shinyImgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${pokeID}.png`;
   
   return (
     <div className='pokemon-card'>
         <div className='pokemon-card-inner'>
-        <img 
-          alt={`Pokemon ${properName}`}
-          name={properName}
-          src={imgSrc} 
-        />
-        <h3>{properName} <span className="pokeID">(#{pokeID})</span></h3>
+          <img 
+            alt={`Pokemon ${properName}`}
+            name={properName}
+            src={imgSrc}
+          />
+          <h3>{properName} <span className="pokeID">(#{pokeID})</span></h3>
       </div>
     </div>
   );
